bb1d5f235cdd7110923b616a859c6f06bd32651d
Zeeshan Younas <136065779+Zeeshan5932@users.noreply.github.com>
2026-02-05 11:58:11 +0500

    langchain changing

--- pdf-chatbot/backend/qa_chain.py
+++ pdf-chatbot/backend/qa_chain.py
@@ -24,7 +24,7 @@
 
 
 from langchain_openai import OpenAIEmbeddings, ChatOpenAI
-from langchain.vectorstores import FAISS
+from langchain_community.vectorstores import FAISS
 from langchain.chains import ConversationalRetrievalChain
 
 def create_qa_chain(chunks, memory):
